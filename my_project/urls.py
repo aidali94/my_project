51c60ec3d53ad3259de50c6ebd41c09bc4b4d7bc
ali <gumarov.ali@gmail.com>
2023-01-11 09:52:51 +0600

    Updated animals app

--- my_project/urls.py
+++ my_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from animals import views as animals
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', animals.index),
+
 ]
