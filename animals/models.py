280f96646a3da5174c8503c0e0fe7e13726c4d60
ali <gumarov.ali@gmail.com>
2023-01-14 12:42:54 +0600

    created animal app

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Animal(models.Model):
+    name = models.CharField(max_length=64)
+    age = models.PositiveSmallIntegerField()
+    description = models.TextField(blank=True)
+    kind = models.ForeignKey('AnimalKind', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'Animal with name: {self.name} and kind: {self.kind}'
+
+
+class AnimalKind(models.Model):
+    kind = models.CharField(max_length=32)
+
+
+class AnimalDetail(models.Model):
+    animal = models.OneToOneField('animal.Animal',
+                                  primary_key=True,
+                                  on_delete=models.CASCADE)
+    biography = models.TextField(blank=True)
--- animals/views.py
+++ animals/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from animals.models import Animal
 
 
 def index(request):
-    pass
-
+    all_animals = Animal.objects.all()
+    print(all_animals)
+    context = {
+        'all_animals': all_animals
+    }
+    return render(
+        request=request,
+        template_name='index.html',
+        context=context
+    )
